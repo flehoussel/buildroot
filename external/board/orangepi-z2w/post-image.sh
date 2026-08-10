e007a80ca8a107919cee398061de8dc12b29a196
Fabien Lehoussel <fabien.lehoussel@smile.fr>
2026-08-10 14:09:01 +0200

    orangepi-z2w: generate the SWUpdate image in post-image
    Builds update_image.swu from the board's sw-description alongside the
    sdcard image, matching how OTA update images are produced for the
    other boards.

--- external/board/orangepi-z2w/post-image.sh
+++ external/board/orangepi-z2w/post-image.sh
@@ -6,3 +6,9 @@ echo ">>> Post-image script arguments <$*>"
 
 echo ">>> Generating sdcard image"
 support/scripts/genimage.sh -c "${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/orangepi-z2w/genimage.cfg"
+
+echo ">>> Generating swu image"
+swugenerator -o "${BINARIES_DIR}/update_image.swu" \
+             -a "${BINARIES_DIR}" \
+             -s "${BR2_EXTERNAL_AA_PROXY_OS_PATH}/board/orangepi-z2w/swupdate/sw-description" \
+             -e create
